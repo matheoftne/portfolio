06af1711ec7cdf18ef057e6812ed70a360da7874
matheoftne <matheo.fontaine.pro@gmail.com>
2024-01-19 14:32:23 +0100

    defer script

---
+++ scripts/script-fade.js
@@ -0,0 +1,24 @@
+ScrollReveal({
+    reset: true,
+    distance: '80px',
+    duration: 1500,
+    delay: 400
+});
+
+ScrollReveal().reveal('#main-title, #second-title', {delay: 500, origin: 'bottom'});
+ScrollReveal().reveal('#quality', {delay: 600, origin: 'left'});
+ScrollReveal().reveal('#cloner', {delay: 800, origin: 'bottom'});
+ScrollReveal().reveal('#section-title', {delay: 500, origin: 'top'});
+ScrollReveal().reveal('.media', {delay: 500, origin: 'top'});
+ScrollReveal().reveal('#section-text', {delay: 500, origin: 'left'});
+ScrollReveal().reveal('#nav', {delay: 200, origin: 'top'});
+ScrollReveal().reveal('.reel_footer', {delay: 200, origin: 'bottom'});
+ScrollReveal().reveal('#images', {delay: 200});
+ScrollReveal().reveal('.js--fadeInRight', {delay: 200, origin: 'right'});
+ScrollReveal().reveal('.js--fadeInLeft', {delay: 300, origin :'left'});
+ScrollReveal().reveal('.timeline', {delay: 300, origin: 'bottom'});
+ScrollReveal().reveal('.marquee', {delay: 300, origin: 'bottom'});
+ScrollReveal().reveal('#btn-contact', {delay: 400, origin: 'left'});
+ScrollReveal().reveal('#stage', {delay: 300, origin: 'bottom'});
+ScrollReveal().reveal('#soon', {delay: 550, origin: 'top'});
+ScrollReveal().reveal('#projets', {delay: 500, origin: 'top'});
